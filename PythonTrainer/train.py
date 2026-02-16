99e48b6dc9c2bdc745eb00e3e0728894e90853d8
the-cyclops <enrico.loda01@gmail.com>
2026-02-16 18:05:56 +0100

    train.py

--- PythonTrainer/train.py
+++ PythonTrainer/train.py
@@ -19,7 +19,7 @@ from colreg_logic import rtamt_yml_parser
 
 model_name = "boat_agent_model_initial"
 # None - use the Unity Editor (press Play)
-unity_env_path = "../Builds/training_gui.app" 
+unity_env_path = "../Builds/train_gui.app" 
 
 #DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 DEVICE = "cpu"
