fe4036125708b99c9896a1ee5e6109b08eb15872
the-cyclops <enrico.loda01@gmail.com>
2026-08-29 18:21:40 +0200

    changed t_coll and t_check

--- PythonTrainer/utils/colreg_handler.py
+++ PythonTrainer/utils/colreg_handler.py
@@ -96,7 +96,7 @@ class COLREGHandler:
         # Positive values (safe) are capped to stabilize Value Network training.
         return min(raw_margin, self.MAX_SAFETY_MARGIN_CAP)
 
-    def get_R1_safety_signal(self, obs, safe_dist=2.0, t_coll=5.0):
+    def get_R1_safety_signal(self, obs, safe_dist=2.0, t_coll=1.0):
         """
         Main function to call in the training loop for Rule R1.
         Returns the worst (minimum) signal value, which will be used by rtamt for robustness calculation.
@@ -112,7 +112,7 @@ class COLREGHandler:
         # Return the critical safety signal (the lowest one)
         return min(signals)
 
-    def get_keep_signal(self, obs, safe_dist=2.0, t_check=10.0, delta_head_on=5.0, max_left_angle=112.5): # 112.5 is the 180-degreeequivalent of 247.5 degrees in 360-degree system
+    def get_keep_signal(self, obs, safe_dist=2.0, t_check=2.0, delta_head_on=5.0, max_left_angle=112.5): # 112.5 is the 180-degreeequivalent of 247.5 degrees in 360-degree system
         """
         Calculates the 'keep' signal (Stand-on vessel status) using STL robustness semantics.
         Logic: Collision Risk AND Intruder in Left Sector (between delta_head_on and max_left_angle)
